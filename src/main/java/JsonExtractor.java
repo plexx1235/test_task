b6f5fbed8a1fbdbf074143d07f9e5aa4991fde22
Denis F <plexx1914@users.noreply.github.com>
2020-03-01 02:20:24 +0200

    renamed extractObjectByName() parameters

--- src/main/java/JsonExtractor.java
+++ src/main/java/JsonExtractor.java
@@ -2,8 +2,8 @@ import com.fasterxml.jackson.databind.JsonNode;
 
 public class JsonExtractor {
 
-    public JsonNode extractObjectByName(JsonNode company, String nodeName) {
-        return company.get(nodeName);
+    public JsonNode extractObjectByName(JsonNode json, String nodeName) {
+        return json.get(nodeName);
     }
 
 }
